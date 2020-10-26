06064d5e37841b1cecdef47a5af9d040c489c47c
Wilmar Florez <wflorez52@gmail.com>
2020-10-25 23:06:22 -0500

    Added link to home at logo

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import { Layout, Menu } from 'antd'
 import { ElenasContext } from '../../contexts/ElenasContext'
+import { Link } from 'react-router-dom'
 
 export const LayoutComponent: React.FC = ({ children }) => {
   const { Header } = Layout
@@ -10,12 +11,12 @@ export const LayoutComponent: React.FC = ({ children }) => {
   return (
     <Layout>
       <Header>
-        <div style={{ float: 'left', color: '#ffffff', fontWeight: 600 }}>
-          Elenas app
+        <div style={{ float: 'left', fontWeight: 500, marginRight: '1em' }}>
+          <Link to='/'>ELENAS APP</Link>
         </div>
         <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
           <Menu.Item key="1" onClick={handleLogout}>
-            Logout
+            LOGOUT
           </Menu.Item>
         </Menu>
       </Header>
